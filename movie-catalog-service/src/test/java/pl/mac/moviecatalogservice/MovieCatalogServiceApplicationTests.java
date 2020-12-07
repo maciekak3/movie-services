6f035457410bce1fd952950887cfe9e38e10f4e3
Maciej Akonom <maciej.akonom@gmail.com>
2020-12-07 16:17:14 +0100

    3 initail projects as the components of the microservices architecture

---
+++ movie-catalog-service/src/test/java/pl/mac/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mac.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
